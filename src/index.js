c97747b2fb9a1159016704958c1ebd1ce4514df3
suraj-turing <suraj.jadhav@turing.com>
2023-11-08 01:35:15 +0530

    feat(app): open dev tools when in development mode

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+require('dotenv').config();
+
+const isDev = process.env.MODE === 'development'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -20,7 +23,8 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  if (isDev)
+    mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
