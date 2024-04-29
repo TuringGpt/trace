2d000e249b6fbdb467c58adae8bc32069ea125e8
Rohit Kumar <ashittheone.98@gmail.com>
2024-04-29 08:41:23 +0530

    [TR-4] update workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ source $NVM_DIR/nvm.sh
 azurite-blob -l ./azure-blob-storage &
 
 # Run the Azure CLI command to create the storage container
-az storage container create --name turing-videos --connection-string "$BLOB_STORAGE_URL"
+az storage container create --name turing-videos --connection-string "${BLOB_STORAGE_URL}"
 
 # Pass control to the main entrypoint script with any arguments passed to the container
 exec "$@"
