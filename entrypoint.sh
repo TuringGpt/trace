e819f15acd1908d522c57b2b1927f28e807e8f61
Rohit Kumar <ashittheone.98@gmail.com>
2024-04-30 17:50:10 +0530

    [TR-4] update workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,7 @@ source $NVM_DIR/nvm.sh
 
 # Run emulator
 azurite-blob -l ./azure-blob-storage &
-echo "SYN1"
-echo "${BLOB_STORAGE_URL}"
-echo "SYN2"
-echo "$BLOB_STORAGE_URL"
+
 # Run the Azure CLI command to create the storage container
 az storage container create --name turing-videos --connection-string "${BLOB_STORAGE_URL}"
 
