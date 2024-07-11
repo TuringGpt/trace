cba4c1c651f3be0a43e2de51cb736a0ece4efb55
suraj-turing <suraj.jadhav@turing.com>
2024-07-12 01:12:14 +0530

    fix: moved sign in with google code into separate component

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -21,6 +21,7 @@ import VideoRecorder from './pages/VideoRecorder';
 import useAppState from './store/hook';
 import AppStateProvider from './store/provider';
 import Logout from './components/Logout';
+import GoogleSignInButton from './components/GoogleSignInButton';
 
 function AppRoutes() {
   const { state } = useAppState();
@@ -54,10 +55,6 @@ export default function App() {
     checkAuthToken();
   }, []);
 
-  const handleGoogleSignIn = () => {
-    window.electron.openGoogleAuth();
-  };
-
   return (
     <AppStateProvider>
       <Router>
@@ -73,29 +70,7 @@ export default function App() {
           ) : (
             <div className="h-full flex flex-col">
               <AppHeader />
-              <div className="flex-grow flex items-center justify-center">
-                <button
-                  type="button"
-                  onClick={handleGoogleSignIn}
-                  className="text-white w-full max-w-xs mx-auto bg-indigo-800 hover:bg-indigo-900 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center transition duration-300 ease-in-out transform hover:-translate-y-1"
-                >
-                  <svg
-                    className="mr-2 -ml-1 w-4 h-4"
-                    aria-hidden="true"
-                    focusable="false"
-                    data-prefix="fab"
-                    data-icon="google"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 488 512"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-                    />
-                  </svg>
-                  Sign in with Google
-                </button>
-              </div>
+              <GoogleSignInButton />
             </div>
           )}
         </div>
