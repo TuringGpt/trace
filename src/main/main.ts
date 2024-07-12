f79a85b0151f1d5c58cd919ee932e77d07bc38d4
suraj-turing <suraj.jadhav@turing.com>
2024-07-12 12:17:29 +0530

    fix: temp

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,21 +1,4 @@
-/* eslint global-require: off, no-console: off, promise/always-return: off */
-
-// Make sure the configLoader is the first import always, so that the env vars are loaded first
-// before any other imports happen
-import './configLoader';
-import './ipc';
-
 import { app, BrowserWindow, ipcMain, shell } from 'electron';
-
-// import { autoUpdater } from 'electron-updater';
-/**
- * This module executes inside of electron's main process. You can start
- * electron renderer process from here and communicate with the other processes
- * through IPC.
- *
- * When running `npm run build` or `npm run build:main`, this file is compiled to
- * `./src/main.js` using webpack. This gives us some performance wins.
- */
 import path from 'path';
 import MenuBuilder from './menu';
 import db from './storage';
@@ -31,22 +14,9 @@ logger.info('App starting...');
 
 setupVideoAndThumbnailHttpServer();
 
-// class AppUpdater {
-//   constructor() {
-//     autoUpdater.logger = log;
-//     autoUpdater.checkForUpdatesAndNotify();
-//   }
-// }
-
 let mainWindow: BrowserWindow | null = null;
 
-if (process.env.NODE_ENV === 'production') {
-  const sourceMapSupport = require('source-map-support');
-  sourceMapSupport.install();
-}
-
-const isDebug =
-  process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
+const isDebug = process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
 
 if (isDebug) {
   require('electron-debug')();
@@ -82,12 +52,6 @@ const createWindow = async () => {
     show: false,
     width: 1300,
     height: 1000,
-    x: process.env.START_WIN_X
-      ? Number.parseInt(process.env.START_WIN_X, 10)
-      : undefined,
-    y: process.env.START_WIN_X
-      ? Number.parseInt(process.env.START_WIN_X, 10)
-      : undefined,
     icon: getAssetPath('icon.png'),
     webPreferences: {
       preload: app.isPackaged
@@ -118,28 +82,26 @@ const createWindow = async () => {
   const menuBuilder = new MenuBuilder(mainWindow);
   menuBuilder.buildMenu();
 
-  // Open urls in the user's browser
   mainWindow.webContents.setWindowOpenHandler((edata) => {
     shell.openExternal(edata.url);
     return { action: 'deny' };
   });
-
-  // Remove this if your app does not use auto updates
-  // eslint-disable-next-line
-  // new AppUpdater();
 };
 
 app.on('ready', () => {
   if (process.defaultApp) {
     if (process.argv.length >= 2) {
-      app.setAsDefaultProtocolClient('trace', process.execPath, [
-        path.resolve(process.argv[1]),
-      ]);
+      app.setAsDefaultProtocolClient('trace', process.execPath, [path.resolve(process.argv[1])]);
     }
   } else {
     app.setAsDefaultProtocolClient('trace');
   }
   createWindow();
+});
+
+app.on('open-url', (event, url) => {
+  event.preventDefault();
+  handleOpenUrl(url);
 });
 
 const handleOpenUrl = (url: string) => {
@@ -152,67 +114,18 @@ const handleOpenUrl = (url: string) => {
     if (accessToken && refreshToken) {
       log.info('OAuth token received');
       setTokens(accessToken, refreshToken);
-      mainWindow.webContents.send('auth-success', {
-        accessToken,
-        refreshToken,
-      });
+      mainWindow.webContents.send('auth-success', { accessToken, refreshToken });
     } else {
       log.error('Error: Missing access token or refresh token');
     }
   }
 };
 
-app.on('open-url', (event, url) => {
-  event.preventDefault();
-  handleOpenUrl(url);
-});
-
-/**
- * Add event listeners...
- */
-
 app.on('window-all-closed', () => {
-  // Respect the OSX convention of having the application in memory even
-  // after all windows have been closed
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
-
-app.setAsDefaultProtocolClient('trace');
-app.on('open-url', (event, url) => {
-  event.preventDefault();
-  if (url.startsWith('trace://auth')) {
-    if (mainWindow) {
-      if (mainWindow.isMinimized()) mainWindow.restore();
-      mainWindow.focus();
-      const parsedUrl = new URL(url);
-      const accessToken = parsedUrl.searchParams.get('token');
-      const refreshToken = parsedUrl.searchParams.get('refreshToken');
-      if (accessToken && refreshToken) {
-        log.info('OAuth token received');
-        setTokens(accessToken, refreshToken);
-        mainWindow.reload();
-      } else {
-        log.error('Error: Missing access token or refresh token');
-      }
-    }
-  }
-});
-
-app
-  .whenReady()
-  .then(() => {
-    createWindow();
-    db.load();
-    UploadManager.getInstance();
-    app.on('activate', () => {
-      // On macOS it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (mainWindow === null) createWindow();
-    });
-  })
-  .catch(log.error);
 
 app.on('before-quit', () => {
   log.info('App about to quit.');
@@ -226,7 +139,6 @@ process.on('unhandledRejection', (reason, promise) => {
   log.error('Unhandled Rejection at:', { promise, reason });
 });
 
-// custom event listener
 ipcMain.on('report-renderer-unhandled-error', (e, error) => {
   log.error('Unhandled Error from renderer:', { error });
 });
